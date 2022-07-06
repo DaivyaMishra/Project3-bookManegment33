4419761a02c02433785eda2380044c8d508bd323
Latha-AJ <lathajappa.7226@gmail.com>
2022-07-06 17:16:47 +0530

    added login user

--- src/controller/UserAPIs.js
+++ src/controller/UserAPIs.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/UserModel')
+const jwt = require("jsonwebtoken")
 
 const createUserAPIs = async function (req, res) {
     try{
@@ -12,19 +13,25 @@ const createUserAPIs = async function (req, res) {
     }
 }
 
- const loginUser = function(req,res){
+ const loginUser = async function(req,res){
    try{
     const credentials = req.body
     const {email, password} = credentials
-    if(Object.keys.length == 0){return res.status(400).send({status: false, message:"Enter your email and password"})}
+    if(Object.keys(credentials).length == 0){return res.status(400).send({status: false, message:"Enter your email and password"})}
     if(!email)  {return res.status(400).send({status:false, message: "Enter your email"})}
     if(!password) {return res.status(400).send({status:false, message:"Enter your password"})}
     
-    const isValid = UserModel.findOne({email:email, password:password })
-    if(isValid == null) {return res.status(400).send({status:false, message:"Invalid credential"})}
+    const isValid =await  UserModel.findOne({email:email, password:password })
+    if(isValid == null) {return res.status(401).send({status:false, message:"Invalid credential"})}
+
+    let token = jwt.sign({userId: isValid._id.toString(), "iat": (new Date().getTime())}, "*Project3_secret_key*" ,{expiresIn: '24h'})
+    res.setHeader("x-api-key",token);
+    res.status(200).send({status:true, data:token})
     
    }catch (err) {
     res.status(500).send({ status: false, msg: err.message })
 }
 }
 module.exports={createUserAPIs,loginUser}
+
+// , expiresIn: '24h'
